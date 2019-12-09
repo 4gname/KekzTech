903c05c9067be4279fcc16542dbc842375c1627e
Lex <domian844@gmail.com>
2019-12-09 17:08:44 +0700

    Revert "Added Item Server recipes. Removed unused reactor part recipes."
    This reverts commit 5fe214204875ac23e078e514dcd4fc8a001e3d37.

--- src/main/java/items/MetaItem_CraftingComponent.java
+++ src/main/java/items/MetaItem_CraftingComponent.java
@@ -14,7 +14,7 @@ import net.minecraft.util.IIcon;
 public class MetaItem_CraftingComponent extends Item {
 	
 	private static MetaItem_CraftingComponent instance = new MetaItem_CraftingComponent();
-	private final IIcon[] icons = new IIcon[17];
+	private final IIcon[] icons = new IIcon[16];
 	
 	private MetaItem_CraftingComponent() {
 		// I am a singleton
@@ -56,7 +56,6 @@ public class MetaItem_CraftingComponent extends Item {
 		icons[counter++] = reg.registerIcon(KekzCore.MODID + ":" + "CeriaDust");
 		icons[counter++] = reg.registerIcon(KekzCore.MODID + ":" + "YSZCeramicPlate");
 		icons[counter++] = reg.registerIcon(KekzCore.MODID + ":" + "GDCCeramicPlate");
-		icons[counter++] = reg.registerIcon(KekzCore.MODID + ":" + "ItemServerBlade");
 	}
 	
 	@Override
